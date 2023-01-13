39abcf485db5a1ec04a1e89bf4ce5453fb45b8fe
Joyce-Wong <joycekwo12@gmail.com>
2023-01-13 13:27:50 -0500

    closestCarrot

---
+++ Structy/Graph/closestCarrot.js
@@ -0,0 +1,117 @@
+// closest carrot
+// Write a function, closestCarrot, that takes in a grid, a starting row, and a starting column. In the grid, 'X's are walls, 'O's are open spaces, and 'C's are carrots. The function should return a number representing the length of the shortest path from the starting position to a carrot. You may move up, down, left, or right, but cannot pass through walls (X). If there is no possible path to a carrot, then return -1.
+
+test_00:
+const grid = [
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'X', 'O', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['O', 'X', 'C', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['C', 'O', 'O', 'O', 'O'],
+];
+
+closestCarrot(grid, 1, 2); // -> 4
+test_01:
+const grid = [
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'X', 'O', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['O', 'X', 'C', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['C', 'O', 'O', 'O', 'O'],
+];
+
+closestCarrot(grid, 0, 0); // -> 5
+test_02:
+const grid = [
+  ['O', 'O', 'X', 'X', 'X'],
+  ['O', 'X', 'X', 'X', 'C'],
+  ['O', 'X', 'O', 'X', 'X'],
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'X', 'X', 'X', 'X'],
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'C', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O'],
+];
+
+closestCarrot(grid, 3, 4); // -> 9
+test_03:
+const grid = [
+  ['O', 'O', 'X', 'O', 'O'],
+  ['O', 'X', 'X', 'X', 'O'],
+  ['O', 'X', 'C', 'C', 'O'],
+];
+
+closestCarrot(grid, 1, 4); // -> 2
+test_04:
+const grid = [
+  ['O', 'O', 'X', 'O', 'O'],
+  ['O', 'X', 'X', 'X', 'O'],
+  ['O', 'X', 'C', 'C', 'O'],
+];
+
+closestCarrot(grid, 2, 0); // -> -1
+test_05:
+const grid = [
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'X'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'C'],
+];
+
+closestCarrot(grid, 0, 0); // -> -1
+test_06:
+const grid = [
+  ['O', 'O', 'X', 'C', 'O'],
+  ['O', 'X', 'X', 'X', 'O'],
+  ['C', 'X', 'O', 'O', 'O'],
+];
+
+closestCarrot(grid, 2, 2); // -> 5
+
+//iterative breadth first
+//r = number of rows
+//c = number of columns
+// time O(rc ^ 2), space O(rc)
+const closestCarrot = (grid, startRow, startCol) => {
+    const visited = new Set([startRow + ',' + startCol])
+    const queue = [[startRow, startCol, 0]]
+    while(queue.length > 0){
+      const [row, column, distance] = queue.shift()
+      
+      if(grid[row][column] === 'C') {
+       return distance
+      }
+      
+      const deltas = [[0, 1],[0, -1],[1, 0],[-1, 0]]
+      
+      for(let delta of deltas){
+        const [rowDelta, columnDelta] = delta
+        const neighborRow = row + rowDelta
+        const neighborColumn = column + columnDelta
+        
+        const rowInBound = 0 <= neighborRow && neighborRow < grid.length
+        const columnInBound = 0 <= neighborColumn && neighborColumn < grid[0].length
+        const neighborPosition = neighborRow + ',' + neighborColumn
+        if(rowInBound && columnInBound && grid[neighborRow][neighborColumn] !== 'X' && !visited.has(neighborPosition)){
+          queue.push([neighborRow, neighborColumn, distance + 1])
+          visited.add(neighborPosition)
+        }
+      }
+      } 
+    return -1
+  }
+  
+  module.exports = {
+    closestCarrot,
+  };
+  
